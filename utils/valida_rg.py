54482dd5f708837f44a3365fce6c74948855d9d3
= <tatimoriam@gmail.com>
2023-08-07 21:35:42 -0300

    :bug: bugfix: validação de RG

--- utils/valida_rg.py
+++ utils/valida_rg.py
@@ -4,8 +4,13 @@ import re
 def valida_rg(rg):
     padrao_rg = r'^\d{2}\.\d{3}\.\d{3}-[0-9A-Za-z]$'
 
+
     while True:
+        rg = re.sub("[-.]", "", rg)
+
+        rg = f"{rg[:2]}.{rg[2:5]}.{rg[5:8]}-{rg[8:]}"
+
         if re.match(padrao_rg, rg):
             return rg
         else:
-            print("RG inválido. Tente novamente.")
+            rg = input("RG inválido. Tente novamente: ")
